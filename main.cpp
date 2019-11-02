dab41511c09563f69892880461b14a3a0cf2c8c6
Boris Goncharov <bora4bora@gmail.com>
2019-11-02 16:41:36 -0700

    main

--- main.cpp
+++ main.cpp
@@ -13,40 +13,56 @@
 #include <iostream>
 #include "NCursesRenderer.hpp"
 
-int main() {
-	NCursesRenderer win;
+int main(int argc, char *argv[]) {
 
-	OSInfoModule os;
-	HostNameModule ho;
-	DateTimeModule da;
-	CPUModule cp;
-	RAMModule ra;
-	NetworkModule ne;
+	std::string inp;
 
-	int key;
-	for (;;) {
-		key = getch();
-		if (key == 27)
-			break ;
-		
-		ho.refresh();
-		ho.render(&win);
-		
-		os.refresh();
-		os.render(&win);
+	if (argc > 1)
+		inp = argv[1];
+	
+	if(inp.compare("-g") == 0) {}
 
-		da.refresh();
-		da.render(&win);
+	else if (inp.compare("-g2") == 0) {}
 
-		cp.refresh();
-		cp.render(&win);
+	else if (inp.compare("-mod1") == 0) {}
 
-		ra.refresh();
-		ra.render(&win);
+	else if (inp.compare("-mod2") == 0) {}
 
-		ne.refresh();
-		ne.render(&win);
-		usleep(1000000);
+	else {
+		NCursesRenderer win;
+
+		OSInfoModule os;
+		HostNameModule ho;
+		DateTimeModule da;
+		CPUModule cp;
+		RAMModule ra;
+		NetworkModule ne;
+
+		int key;
+		for (;;) {
+			key = getch();
+			if (key == 27)
+				break ;
+			
+			ho.refresh();
+			ho.render(&win);
+			
+			os.refresh();
+			os.render(&win);
+
+			da.refresh();
+			da.render(&win);
+
+			cp.refresh();
+			cp.render(&win);
+
+			ra.refresh();
+			ra.render(&win);
+
+			ne.refresh();
+			ne.render(&win);
+			usleep(1000000);
+		}
 	}
 
 	return (0);
