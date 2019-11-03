81765c4ffdbfa3f4bcd061e9be8d14332fe82e52
nimfa27 <kseniia.denisova@outlook.com>
2019-11-02 18:58:57 -0700

    usage and config

--- main.cpp
+++ main.cpp
@@ -26,23 +26,43 @@ std::vector<IMonitorModule*> getSelectedModules() {
 	return (m);
 }
 
-int main() {
-	NCursesRenderer win;
+void putUsage(char *str) {
+	std::cout << "usage: " << str << " [-tg]" << std::endl << std::endl;
+	std::cout << " t : text mode" << std::endl;
+	std::cout << " g : graphical mode" << std::endl << std::endl;
+}
 
-	std::vector<IMonitorModule*> modules = getSelectedModules();
-	
-	int key;
-	for (;;) {
-		key = getch();
-		if (key == 27)
-			break ;
-
-		for (std::vector<IMonitorModule*>::iterator it = modules.begin(); it != modules.end(); it++) {
-			(*it)->refresh();
-			(*it)->render(&win);
-		}
-		usleep(1000000);
+int main(int argc, char **argv) {
+	if (argc < 2) {
+		putUsage(argv[0]);
+		return (0);
 	}
+	std::string mode = argv[1];
 
+	if (mode.compare("-t") == 0) {
+		NCursesRenderer win;
+
+		std::vector<IMonitorModule*> modules = getSelectedModules();
+		
+		int key;
+		for (;;) {
+			key = getch();
+			if (key == 27)
+				break ;
+
+			for (std::vector<IMonitorModule*>::iterator it = modules.begin(); it != modules.end(); it++) {
+				(*it)->refresh();
+				(*it)->render(&win);
+			}
+			usleep(1000000);
+		}
+	}
+	else if (mode.compare("-g") == 0) {
+		
+	}
+	else {
+			putUsage(argv[0]);
+	}
+		
 	return (0);
 }
